9db4d90bb1554e031f5292ba1598d30f62af07aa
Kaze Wong <kazewong.physics@gmail.com>
2025-01-09 03:29:48 +0000

    scaffold basic flask server. One can use start and record end point to record

--- main.py
+++ main.py
@@ -1,21 +1,59 @@
 from flask import Flask
 from flask_cors import CORS
 from flask import jsonify
+from time import time
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+
+class VideoHandler:
+    def __init__(self):
+        self.picam2 = None
+        self.encoder = None
+        self.output = None
+    
+    def update(self):
+        self.picam2 = Picamera2()
+        video_config = self.picam2.create_video_configuration()
+        self.picam2.configure(video_config)
+        self.encoder = H264Encoder(10000000)
+        self.output = 'test.h264'
+
+    def start(self):
+        self.picam2.start_recording(self.encoder, self.output)
+
+    def stop(self):
+        self.picam2.stop_recording()
+        self.picam2.close()
+        
+    def close(self):
+        self.picam2.close()
 
 app = Flask(__name__)
 CORS(app)
 
+
+videoHandler = VideoHandler()
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
 @app.route('/start')
 def start():
-    return jsonify({'message': 'Hello World!'})
+    videoHandler.update()
+    videoHandler.start()
+    app.logger.info('Start recording at ' + str(time()))
+    return jsonify({'message': 'Started recording'})
 
 @app.route('/pause')
-def pause():
-    return jsonify({'message': 'Pause'})
+def stop():
+    videoHandler.stop()
+    app.logger.info('Stop recording at ' + str(time()))
+    return jsonify({'message': 'Finish recording'})
 
 @app.route('/collect')
 def collect():
     return jsonify({'message': 'Collect'})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", debug=True)
