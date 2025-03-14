fa3c1e61344f16fa3e1879896146c5fcae3732e7
Kaze Wong <kazewong.physics@gmail.com>
2025-03-14 10:36:49 -0400

    refactor video handler to improve configuration and formatting consistency

--- main.py
+++ main.py
@@ -8,29 +8,34 @@ from picamera2.encoders import H264Encoder
 from picamera2.outputs import FfmpegOutput
 from libcamera import controls
 
+
 class CAMERA_STATUS(Enum):
     IDLE = 0
     RECORDING = 1
     PAUSED = 2
     DISCONNECTED = 3
 
+
 class VideoHandler:
-    def __init__(self,
-                 tag: str):
+    def __init__(self, tag: str):
         self.tag = tag
         self.picam2 = None
         self.encoder = None
         self.output = None
         self.status = CAMERA_STATUS.DISCONNECTED
-    
+
     def update(self):
         self.picam2 = Picamera2()
-        video_config = self.picam2.sensor_modes[1]
-        assert video_config['fps'] > 30, "fps must be greater than 30"
+        sensor_mode = self.picam2.sensor_modes[1]
+        video_config = self.picam2.create_video_configuration(
+            sensor={"output_size": sensor_mode["size"]}
+        )
         self.picam2.configure(video_config)
-        self.picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+        self.picam2.set_controls(
+            {"AfMode": controls.AfModeEnum.Continuous, "FrameRate": sensor_mode["fps"]}
+        )
         self.encoder = H264Encoder(10000000)
-        self.output = self.tag+'_'+str(int(time()))+'.h264'
+        self.output = self.tag + "_" + str(int(time())) + ".h264"
 
     def start(self):
         self.picam2.start_recording(self.encoder, self.output)
@@ -38,53 +43,62 @@ class VideoHandler:
     def stop(self):
         self.picam2.stop_recording()
         self.picam2.close()
-        
+
     def close(self):
         self.picam2.close()
+
 
 app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 
-videoHandler = VideoHandler('raspi_cam1')
+videoHandler = VideoHandler("raspi_cam1")
 
-@app.route('/')
+
+@app.route("/")
 def hello():
-    return 'Hello, World!'
+    return "Hello, World!"
 
-@app.route('/download')
+
+@app.route("/download")
 def download():
     if videoHandler.output is None:
-        return 'No video to download'
+        return "No video to download"
     return send_file(videoHandler.output, as_attachment=True)
 
-@socketio.on('connect')
+
+@socketio.on("connect")
 def handle_connect():
-    print('client connected')
+    print("client connected")
     videoHandler.status = CAMERA_STATUS.IDLE
 
-@socketio.on('ping')
+
+@socketio.on("ping")
 def handle_ping():
-    print('received ping')
+    print("received ping")
     return time()
 
-@socketio.on('start_recording')
+
+@socketio.on("start_recording")
 def handle_start_recording():
-    print('Start recording at ' + str(time()))
+    print("Start recording at " + str(time()))
     videoHandler.update()
     videoHandler.start()
     videoHandler.status = CAMERA_STATUS.RECORDING
 
-@socketio.on('pause_recording')
+
+@socketio.on("pause_recording")
 def handle_pause_recording():
     raise NotImplementedError
 
-@socketio.on('stop_recording')
+
+@socketio.on("stop_recording")
 def handle_stop_recording():
-    print('Stop recording at ' + str(time()))
+    print("Stop recording at " + str(time()))
     videoHandler.stop()
     videoHandler.status = CAMERA_STATUS.IDLE
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", debug=True)
